6f33f0f9ed6c59f5c912ab4d1ebe326760fd8c27
Preethi-23102000 <preethi.yennemadi@gmail.com>
2026-06-22 18:04:16 -0400

    chore: scaffold multi-module maven project

---
+++ queueforge-api/src/main/java/com/queueforge/api/QueueForgeApiApplication.java
@@ -0,0 +1,12 @@
+package com.queueforge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueForgeApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QueueForgeApiApplication.class, args);
+  }
+}
---
+++ queueforge-worker/src/main/java/com/queueforge/worker/QueueForgeWorkerApplication.java
@@ -0,0 +1,12 @@
+package com.queueforge.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueForgeWorkerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QueueForgeWorkerApplication.class, args);
+  }
+}
